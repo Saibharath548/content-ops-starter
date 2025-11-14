4ecce661a3a6a95ec0df42f7e5fd08986fd882a6
Saibharath548 <kattavsbharath2004@gmail.com>
2025-11-14 14:26:51 +0530

    Revert allowedDevOrigins to strings to avoid RegExp error

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // keep your existing config keys here...
+    // other config keys...
 
     allowedDevOrigins: [
         'https://devserver-main--ishunani-indie.netlify.app',
         'http://localhost:3000',
         'http://localhost:8888',
-        // also allow any devserver preview hosts on netlify.app (regex)
-        /^https:\/\/devserver-.*\.netlify\.app$/,
     ],
 };
 
